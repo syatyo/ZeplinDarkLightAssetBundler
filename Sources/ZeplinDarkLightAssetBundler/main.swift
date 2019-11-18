f7339a12244333980a445015189623d8d8dd5eba
syatyo <yoshiharuseijin@gmail.com>
2019-11-19 01:14:02 +0900

    tmp

--- Sources/ZeplinDarkLightAssetBundler/main.swift
+++ Sources/ZeplinDarkLightAssetBundler/main.swift
@@ -7,3 +7,18 @@
 
 import Foundation
 import SPMUtility
+
+// arguments[0] is command name
+let arguments = Array(CommandLine.arguments.dropFirst())
+
+let parser = ArgumentParser(usage: "-i [input] -o [output]", overview: "Bundle colored assets from input to output")
+
+let inputKeyword = parser.add(option: "--input", shortName: "-i", kind: String.self)
+let outputKeyword = parser.add(option: "--output", shortName: "-o", kind: String.self)
+
+do {
+    let result = try parser.parse(arguments)
+    if let inputKeyword = result.get(inputKeyword), let outputKeyword = result.get(outputKeyword) {
+        
+    }
+}
