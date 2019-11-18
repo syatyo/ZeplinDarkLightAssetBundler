918bade6e4c2bdb8e920218f0fc733a4bb6d25b4
syatyo <yoshiharuseijin@gmail.com>
2019-11-19 01:37:13 +0900

    feat: 🎸 Implement cli

--- Sources/ZeplinDarkLightAssetBundler/main.swift
+++ Sources/ZeplinDarkLightAssetBundler/main.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SPMUtility
+import ZeplinDarkLightAssetBundlerCore
 
 // arguments[0] is command name
 let arguments = Array(CommandLine.arguments.dropFirst())
@@ -16,9 +17,33 @@ let parser = ArgumentParser(usage: "-i [input] -o [output]", overview: "Bundle c
 let inputKeyword = parser.add(option: "--input", shortName: "-i", kind: String.self)
 let outputKeyword = parser.add(option: "--output", shortName: "-o", kind: String.self)
 
+func exitProcessAsFailure() -> Never {
+    exit(1)
+}
+
+func exitProcessAsSuccess() -> Never {
+    exit(0)
+}
+
 do {
     let result = try parser.parse(arguments)
-    if let inputKeyword = result.get(inputKeyword), let outputKeyword = result.get(outputKeyword) {
-        
+    
+    guard let inputKeyword = result.get(inputKeyword) else {
+        print("Can not get input path from :\(result.description)")
+        exitProcessAsFailure()
     }
+    
+    guard let outputKeyword = result.get(outputKeyword) else {
+        print("Can not get output path from :\(result.description)")
+        exitProcessAsFailure()
+    }
+    
+    let inputURL = URL(fileURLWithPath: inputKeyword)
+    let outputURL = URL(fileURLWithPath: outputKeyword)
+    
+    let bundler = ZeplinDarkLightAssetBundler(sourceXCAssetURL: inputURL, targetXCAssetURL: outputURL)
+    try bundler.execute()
+    
+} catch {
+    print(error)
 }
